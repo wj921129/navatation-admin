463bc35233891659265d023aa0cfe1be4ab5027d
wj921129 <631960490@qq.com>
2026-06-08 17:46:43 +0800

    dev: auto update code

--- ddl.sql
+++ ddl.sql
@@ -174,7 +174,7 @@ CREATE TABLE IF NOT EXISTS `navatation_user_widget` (
     `updated_at`     DATETIME              NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
     PRIMARY KEY (`row_id`),
     UNIQUE KEY `uk_widget_id` (`widget_id`),
-    KEY `idx_user_id` (`user_id`),
+    KEY `idx_user_id` (`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 CREATE TABLE IF NOT EXISTS `navatation_root_config` (
